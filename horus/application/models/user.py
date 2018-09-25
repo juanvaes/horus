d91632dfb9a83541951f3bc7212f2b66a0782917
juanvaes <juanvaes22@gmail.com>
2018-09-25 18:30:09 -0500

    Adding a first user model

---
+++ horus/application/models/user.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from ..app import Base
+
+class User(Base):
+    __tablename__ = 'users'
+    
+    id = Column(Integer, primary_key=True)
+    fname = Column(String)
+    lname = Column(String)
+    username = Column(String)
+    email = Column(String)
+    password = Column(String)
+
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = password
+
+    def __repr__(self):
+        return "<User(username={}, email={})".format(self.username, self.email)
+
