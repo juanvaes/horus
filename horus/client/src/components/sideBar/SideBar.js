445571b38035888cf289b72e646f66d7a96e543d
juanvaes <macbookpro@McBookPro.local>
2018-10-17 14:37:51 -0500

    Create Navlinks into sideBar component

--- horus/client/src/components/sideBar/SideBar.js
+++ horus/client/src/components/sideBar/SideBar.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import {Navbar} from 'reactstrap'
+import {Nav, NavItem, NavLink} from 'reactstrap'
 
 class SideBar extends React.Component {
     render() {
         return(
             <div>
                 <h3>Menú</h3>
-                <a href="/">Inicio</a>
-                <a href="/client">Usuarios</a>
-                <a href="/payment">Pagos</a>
+                <Nav vertical>
+                    <NavItem>
+                        <NavLink href="/">Inicio</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="/client">Usuarios</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="/payment">Pagos</NavLink>
+                    </NavItem>
+                </Nav>
+
             </div>
         );
     }
