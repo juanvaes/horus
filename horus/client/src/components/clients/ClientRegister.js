e576fab00074cbfced50784d9c28295fc695ef3c
juancamilo <jcvalencia22@hotmail.com>
2018-10-16 19:36:48 -0500

    Client and payment component

---
+++ horus/client/src/components/clients/ClientRegister.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class ClientRegister extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>Register Client</h1>
+            </div>
+        )
+    }
+
+}
+
+export default ClientRegister;
---
+++ horus/client/src/components/payments/Payments.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Payments extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>Payments</h1>
+            </div>
+        )
+    }
+
+}
+
+export default Payments;
