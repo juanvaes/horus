97149d6ca3a6beaebf8f6bbe64177908b43e62a3
juancamilo <jcvalencia22@hotmail.com>
2019-01-03 22:20:57 -0500

    Add form and functions to it

--- horus/client/src/components/clients/ClientRegister.js
+++ horus/client/src/components/clients/ClientRegister.js
@@ -1,10 +1,29 @@
 import React from 'react';
 
 class ClientRegister extends React.Component{
+    state = {
+        fname: "",
+        lname: ""
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            fname: e.target.value
+        });
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault(); {/*Prevents the default behaviour of refresh the page when submit */}
+        console.log('Form submitted', this.state.fname)
+    }
     render(){
         return(
             <div>
-                <h1>Register Client</h1>
+                <h1>Registering Client with name {this.state.fname}</h1>
+                <form onSubmit={this.handleSubmit}> {/* OnSubmit takes the event when a user clicks on submit button or press enter */}
+                    <input type="text" onChange={this.handleChange}/>
+                    <button>Submit</button>
+                </form>
             </div>
         )
     }
